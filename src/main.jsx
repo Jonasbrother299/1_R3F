5d2bb3a5b7b0681743d12bd6b67613ab04cda3fd
Your Name <you@example.com>
2023-03-14 19:23:02 +0100

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import { React, StrictMode } from "react";
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+import "./styles.css"
+
+import { Canvas } from "@react-three/fiber";
+
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+
+root.render(
+  <StrictMode>
+
+
+  </StrictMode>
+)
